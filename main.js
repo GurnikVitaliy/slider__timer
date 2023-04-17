79377bb7cdf203162da807c140beaef79a34df85
GurnikVitaliy <111244081+GurnikVitaliy@users.noreply.github.com>
2023-04-17 19:20:13 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
-const wrapperDay = document.querySelectorAll('.wrapperDay');
-let index = 0;
+const apiKey = '21dadd8e1c94407e90b143448231704';
 
-const actv = n => {
-    for(let slide of wrapperDay) {
-        slide.classList.remove('active');
-    }
-    wrapperDay[n].classList.add('active');
-}
+// http://api.weatherapi.com/v1/current.json?key=21dadd8e1c94407e90b143448231704&q=London
 
-setInterval( () => {
-    if(index === wrapperDay.length - 1) {
-        index = 0;
-        actv(index);
-    }
-    else {
-        index++;
-        actv(index);
-    }
-},5000);
+
+
+const btn = document.querySelector('#btn');
+const inp = document.querySelector('#inp');
+const main = document.querySelector('.main');
+
+let city;
+
+btn.addEventListener('click', () => {
+    city = inp.value.trim();
+   
+const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`
+
+fetch(url).then((response) => {
+    return response.json()
+}).then((data) => {
+    const html = `<div id='card'>
+    <h2> город - ${data.location.name}</h2>
+    <p id='p'>температура - ${data.current.temp_c}</p>
+    </div>
+    
+    `
+    main.innerHTML = html;
+  })
+})
